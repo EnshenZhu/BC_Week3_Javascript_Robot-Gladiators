94da010e351c357905e84d1ba696bb7a2facdad9
WilsonZhu <winniecmz.zhu@gmail.com>
2022-01-11 17:31:27 -0500

    add the fighting process to the fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,35 @@
-var playerName=window.prompt("What is your robot's name?")
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName)
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun! The player's robot "+playerName+" comes online.");
-  }
+var fight = function() {
 
-//   fight();
+  // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth -= playerAttack;
+   
+  // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth -= enemyAttack
+
+  // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+};
+
+fight();
