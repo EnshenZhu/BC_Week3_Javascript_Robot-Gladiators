777a610f9720028278d7b6c3fcf0525fb189a3e0
WilsonZhu <winniecmz.zhu@gmail.com>
2022-01-17 03:51:25 -0500

    finish all works on 3_1

---
+++ assets/js/test_bench.js
@@ -0,0 +1,7 @@
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
