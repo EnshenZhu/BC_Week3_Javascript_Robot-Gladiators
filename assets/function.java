333d72f119ace8aa291df20fe822b512ef1be51a
WilsonZhu <winniecmz.zhu@gmail.com>
2022-01-18 01:40:52 -0500

    finish all works

---
+++ assets/function.java
@@ -0,0 +1,12 @@
+package assets;
+
+public class function{
+    
+    public static void main(String[] arg){
+        int x= 1;
+        String a="test string";
+        System.out.println(x);
+        System.out.println(a);
+    }   
+    
+}
